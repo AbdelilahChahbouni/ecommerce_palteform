b73d80ea71bc3deec565f872f681e3fe910fa8ee
AbdelilahChahbouni <abdelchahbouni@live.fr>
2025-01-30 17:17:47 +0100

    create Account model and make migrate

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager
-
+from django.contrib.auth.models import BaseUserManager , AbstractBaseUser 
+import pycountry
 
 class MyAccountManager(BaseUserManager):
     def create_user(self , f_name , l_name , user_name , email , country , password):
@@ -34,3 +34,42 @@ class MyAccountManager(BaseUserManager):
         user.save(using= self._db)
         return user
     
+
+
+class Account(AbstractBaseUser):
+    @staticmethod
+    def get_country():
+        countries = list(pycountry.countries)
+        country_choices = [(country.alpha_2 , country.name) for country in countries]
+        return country_choices
+    
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=100 , unique=True)
+    username = models.CharField(max_length=100 , unique= True)
+    phone_number = models.CharField(max_length=50)
+    country = models.CharField(max_length=2 , choices=get_country() , default="US")
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FILEDS = ['last_name' , 'first_name']
+
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return self.username
+    
+    def has_perm(self , perm , obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self , app_label):
+        return True
